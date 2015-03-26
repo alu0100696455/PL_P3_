e39d54960d5bd4a008c4fa35193bf957c16f1344
alu0100696455 <alu0100696455@ull.edu.es>
2015-03-26 20:31:46 +0000

    Añadido jquery

--- public/main.js
+++ public/main.js
@@ -1,3 +1,11 @@
+"use strict";
+
+$(document).ready(function() {
+    $("#PARSE").click(function() {
+      main();
+    });
+ });
+
 Object.prototype.error = function (message, t) {
     t = t || this;
     t.name = "SyntaxError";
@@ -9,11 +17,10 @@ function main() {
     var parse = make_parse();
 
 
-    var source = INPUT.value;
+    var source = document.getElementById('INPUT').value;
     var string, tree;
     try {
         tree = parse(source);
-
         //string = JSON.stringify(tree, ['type', 'value', 'from', 'to'],  4);
         string = JSON.stringify(tree, ['key', 'name', 'message',
              'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
@@ -21,9 +28,5 @@ function main() {
         string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
                 'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
     }
-    OUTPUT.innerHTML = string.replace(/&/g, '&amp;').replace(/[<]/g, '&lt;');
+    document.getElementById('OUTPUT').innerHTML = string.replace(/&/g, '&amp;').replace(/[<]/g, '&lt;');
 };
-
-window.onload = function() {
-  PARSE.onclick = main;
-}
