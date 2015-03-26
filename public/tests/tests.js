2f1677625812d4fb389c2bf3e770de152f06d5f2
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-03-26 22:04:27 +0000

    :full_moon_with_face: Añadiendo tests :new_moon_with_face:

--- public/tests/tests.js
+++ public/tests/tests.js
@@ -1,33 +1,73 @@
 var assert = chai.assert;
 
-suite('csv', function() {
+suite('Analizador Lexico', function() {
 
-  setup(function(){
-    if (typeof __html__ !== 'undefined') {
-      document.body.innerHTML = __html__['tests/index.html'];
-      original = document.getElementById('original');
-      finaltable = document.getElementById('finaltable');
+  test('Se parsea correctamente.', function() {
+    var parse = make_parse();
+
+    var source = "var a = 3 + 3;";
+    var string, tree;
+    try {
+        tree = parse(source);
+        string = JSON.stringify(tree, ['key', 'name', 'message',
+            'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    } catch (e) {
+        string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+                'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
     }
+
+    assert.deepEqual(string, '{\n    "value": "=",\n    "arity": "binary",\n    "first": {\n        "value": "a",\n        "arity": "name"\n    },\n    "second": {\n        "value": "+",\n        "arity": "binary",\n        "first": {\n            "value": 3,\n            "arity": "literal"\n        },\n        "second": {\n            "value": 3,\n            "arity": "literal"\n        }\n    }\n}');
   });
 
-  test('Dos filas con tres columnas', function() {
-    original.value = 'a, 1, 2 \nb, 3, 4';
-    setTimeout(function() {
-      assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>a</td>                                  <td> 1</td>                                  <td> 2 </td>              </tr>\n<tr>                    <td>b</td>                                  <td> 3</td>                                  <td> 4</td>              </tr>\n</tbody></table>');
-    }, 5000);
+  test('Parseo de sentencia if.', function() {
+    var parse = make_parse();
+
+    var source = "var a = true; \n var aux = 0; \n if(a) { \n aux += 3; \n }";
+    var string, tree;
+    try {
+        tree = parse(source);
+        string = JSON.stringify(tree, ['key', 'name', 'message',
+            'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    } catch (e) {
+        string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+                'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    }
+
+    assert.deepEqual(string, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": true,\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "aux",\n            "arity": "name"\n        },\n        "second": {\n            "value": 0,\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "if",\n        "arity": "statement",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": "+=",\n            "arity": "binary",\n            "first": {\n                "value": "aux",\n                "arity": "name"\n            },\n            "second": {\n                "value": 3,\n                "arity": "literal"\n            }\n        },\n        "third": null\n    }\n]');
+    });
+
+  test('Se parsea el contenido inicial del cuadro de texto', function() {
+    var parse = make_parse();
+
+    var source = document.getElementById('INPUT').value;
+    var string, tree;
+    try {
+        tree = parse(source);
+        string = JSON.stringify(tree, ['key', 'name', 'message',
+             'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    } catch (e) {
+        string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+                'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    }
+
+    assert.deepEqual(string, '[\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "a",\n            "arity": "name"\n        },\n        "second": {\n            "value": "hello",\n            "arity": "literal"\n        }\n    },\n    {\n        "value": "=",\n        "arity": "binary",\n        "first": {\n            "value": "b",\n            "arity": "name"\n        },\n        "second": {\n            "value": "function",\n            "arity": "function",\n            "first": [\n                {\n                    "value": "x",\n                    "arity": "name"\n                }\n            ],\n            "second": [\n                {\n                    "value": "=",\n                    "arity": "binary",\n                    "first": {\n                        "value": "c",\n                        "arity": "name"\n                    },\n                    "second": {\n                        "value": 3,\n                        "arity": "literal"\n                    }\n                },\n                {\n                    "value": "return",\n                    "arity": "statement",\n                    "first": {\n                        "value": "+",\n                        "arity": "binary",\n                        "first": {\n                            "value": "x",\n                            "arity": "name"\n                        },\n                        "second": {\n                            "value": "c",\n                            "arity": "name"\n                        }\n                    }\n                }\n            ]\n        }\n    }\n]');
   });
 
-  test('Dos filas con distinto número de columnas', function() {
-    original.value = 'a, 1, 2 \nb, 3, 4, 5';
-    setTimeout(function() {
-      assert.match(finaltable.innerHTML, /error/);
-    }, 5000);
-  });
+  test('Se envia mensaje de error de sintaxis', function() {
+    var parse = make_parse();
 
-  test('Fila vacía', function() {
-    original.value = 'a, 1, 2 \nb, 3, 4\n';
-    setTimeout(function() {
-      assert.deepEqual(finaltable.innerHTML, '<p>\n</p><table class="center" id="result">\n<tbody><tr>                    <td>a</td>                                  <td> 1</td>                                  <td> 2 </td>              </tr>\n<tr>                    <td>b</td>                                  <td> 3</td>                                  <td> 4</td>              </tr>\n</tbody></table>');
-    }, 5000);
-  });
+    var source = "pepe";
+    var string, tree;
+    try {
+        tree = parse(source);
+        string = JSON.stringify(tree, ['key', 'name', 'message',
+             'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    } catch (e) {
+        string = JSON.stringify(e, ['name', 'message', 'from', 'to', 'key',
+                'value', 'arity', 'first', 'second', 'third', 'fourth'], 4);
+    }
+
+    assert.deepEqual(string, '{\n    "name": "SyntaxError",\n    "message": "Undefined.",\n    "from": 0,\n    "to": 4,\n    "value": "pepe",\n    "arity": "name"\n}');
+    });
+
 });
